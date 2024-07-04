78a87b809af5bf9bf45ad50a481225e5491397ba
BastD29 <bastiendaniel9@gmail.com>
2024-07-04 18:03:46 +0200

    added scripts in package.json & removed SESSION_SECRET_KEY

--- server/src/types/dotenv.d.ts
+++ server/src/types/dotenv.d.ts
@@ -2,7 +2,6 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: "development" | "production" | "test" | "local" | "staging";
     PORT: string;
-    SESSION_SECRET_KEY: string;
     MONGO_URI: string;
     JWT_SECRET: string;
     JWT_KEY: string;
